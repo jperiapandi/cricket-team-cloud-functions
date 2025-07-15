fa1ef486409cd84d8d1758dcf88035bf7403ad96
Periapandi Jeyaram <jperiapandi@gmail.com>
2025-07-15 18:26:21 +0530

    cors vlaues added

--- functions/index.js
+++ functions/index.js
@@ -7,10 +7,10 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 // const logger = require("firebase-functions/logger");
-const {getFirestore} = require("firebase-admin/firestore");
-const {initializeApp} = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const { initializeApp } = require("firebase-admin/app");
 
 initializeApp();
 
@@ -22,20 +22,29 @@ initializeApp();
 //   response.send("Hello from Firebase!");
 // });
 
-exports.getPlayers = onRequest(async (request, response) => {
-  try {
-    const playersQSnapshot = await getFirestore().collection("players").get();
-    if (playersQSnapshot.empty) {
-      response.status(404).send("No Data");
-      return;
+exports.getPlayers = onRequest(
+  {
+    cors: [
+      "https://cricket-team-creator.web.app",
+      /firebase\.com$/,
+      /localhost:[0-9]{4}/,
+    ],
+  },
+  async (request, response) => {
+    try {
+      const playersQSnapshot = await getFirestore().collection("players").get();
+      if (playersQSnapshot.empty) {
+        response.status(404).send("No Data");
+        return;
+      }
+
+      const players = playersQSnapshot.docs.map((d) => {
+        return d.data();
+      });
+
+      response.status(200).json({ players });
+    } catch (err) {
+      response.status(500).send(err.message || "Something went wrong.");
     }
-
-    const players = playersQSnapshot.docs.map((d) => {
-      return d.data();
-    });
-
-    response.status(200).json({players});
-  } catch (err) {
-    response.status(500).send(err.message || "Something went wrong.");
-  }
-});
+  },
+);
